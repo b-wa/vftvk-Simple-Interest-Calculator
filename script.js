13cb2cc38849c29566c86cda57ea61022bc2c9d2
b-wa <bmay.wanjiru@gmail.com>
2021-06-21 03:45:14 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years); 
+
+ 
+
+    var result = document.getElementById("result");
+
+    if (principal <= 0) {
+        alert("Enter a positive number")
+    } else {
+        return result;
+    }
+
+    result.innerHTML = "If you deposit " + principal + " at an interest rate of " + rate + "% " + " you will receive an amount of " + interest + " in the year " + year
 }
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval; 
+
+}
+
+function principal() {
+  var x = document.forms["myForm"]["fname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+}
+
         
